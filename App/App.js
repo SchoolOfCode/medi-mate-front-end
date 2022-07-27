5958f52407ab76139f0c765a802d3567c00be79f
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-07-27 12:00:22 +0100

    added some file structure for landing page and auth0

--- App/App.js
+++ App/App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Auth0 from '../LandingPage/Auth0/Auth0';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Auth0></Auth0>
+      <landingPage></landingPage> // for when
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
